ed9570f7c73c82b8d91d92824efce001640c8f6a
Jessica Greene <jessica0greene@gmail.com>
2017-09-18 18:10:15 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = { 
+queen: "Killer Queen",
+coral: "In my Room",
+fooFighters: "All my Life"}
+
+function updatePlaylist(obj, artist, song){
+  obj.artist = song;
+  return obj
+}
+
+function removeFromPlaylist(obj,artist){
+  delete obj.artist;
+  return obj
+}
